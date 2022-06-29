2117d30387b65f3d39812032c099f7ccf083f12d
Gulzadadzh <Dzhusukeeva@gmail.com>
2022-06-29 12:16:58 -0400

    class 9

--- com/syntax/class08/recap.java
+++ com/syntax/class08/recap.java
@@ -3,16 +3,6 @@ package com.syntax.class08;
 public class recap {
 	public static void main(String[] args) {
 
-	//We are counting 1-10
-	//We want to stop the loop when reach 4
-		
-		
-		for(int i=1; i<=10; i++) {
-			if(i ==4) {
-				break;
-			}
-			System.out.println(i);
-		}
 		
 		
 		
