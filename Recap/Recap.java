ddff742dc60c71a2fc3794b7e15f1465f9181753
Gulzadadzh <Dzhusukeeva@gmail.com>
2022-08-26 10:45:07 -0400

    recap

---
+++ Recap/Recap.java
@@ -0,0 +1,22 @@
+package Recap;
+
+public class Recap {
+	public static void main(String[] args) {
+		
+		int i=0;
+		
+		while(i<6) {
+			System.out.print("Hi");
+			i++;
+		}
+		
+		
+		int b= 3;
+		
+		while(b<=3) {
+			b++;
+			System.out.println("nnnn");
+		}
+	}
+
+}
